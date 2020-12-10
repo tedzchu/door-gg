9e36e072e9b2c04bafe1b83d5a307a736ba61972
it-door <tedchu4@gmail.com>
2020-12-10 02:04:11 -0800

    Build fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home, Migada } from './views';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/migada" component={Migada} />
+      <Route path="/migada" component={Migada} />
     </Switch>
   );
 };
